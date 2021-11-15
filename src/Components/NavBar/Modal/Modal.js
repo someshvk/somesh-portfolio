cd3085d341b69bdb1bc51a595ff213ca026018fd
someshvk <svkhandelwal655@gmail.com>
2021-11-15 16:31:59 +0530

    add scroll effect

--- src/Components/NavBar/Modal/Modal.js
+++ src/Components/NavBar/Modal/Modal.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './modal.css';
+import {Link} from 'react-scroll';
 
 const Modal = ({changeModal}) => {
-    const changeWindow = (position) =>{
-        window.location.replace(position);
-        changeModal();
-    }
+    // const changeWindow = (position) =>{
+    //     window.location.replace(position);
+    //     changeModal();
+    // }
     return (
-        <React.Fragment>
+        <>
             <div className="modalBg" >
                 <span className="modalContent">
-                    <span className="menu homeMenu" onClick={() => changeWindow("/#home")}><span className="highlightedText">1. </span>Home</span>
-                    <span className="menu aboutMenu" onClick={() => changeWindow("/#about")}><span className="highlightedText">2. </span>About</span>
-                    <span className="menu workMenu" onClick={() => changeWindow("/#work")}><span className="highlightedText">3. </span>Work</span>
-                    <span className="menu contactMenu" onClick={() => changeWindow("/#contact")}><span className="highlightedText">4. </span>Contact</span>
+                    <Link activeClass="active" to="home" spy={true} smooth={true}><span className="menu homeMenu" onClick={() => changeModal()}><span className="highlightedText">1. </span>Home</span></Link>
+                    <Link to="about" spy={true} smooth={true}><span className="menu aboutMenu" onClick={() => changeModal()}><span className="highlightedText">2. </span>About</span></Link>
+                    <Link to="work" spy={true} smooth={true}><span className="menu workMenu" onClick={() => changeModal()}><span className="highlightedText">3. </span>Work</span></Link>
+                    <Link to="contact" spy={true} smooth={true}><span className="menu contactMenu" onClick={() => changeModal()}><span className="highlightedText">4. </span>Contact</span></Link>
                 </span>
             </div>
-        </React.Fragment>
+        </>
     );
 }
-
+// 1056-4E4B-5E3C-9DA1-3FCF-315F-DC92-3AF8
 export default Modal;
