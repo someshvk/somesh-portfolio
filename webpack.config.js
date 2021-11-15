5f75353b4fc23ff18d1ab13cd87d524bbe9a9e4c
someshvk <svkhandelwal655@gmail.com>
2021-11-15 15:01:12 +0530

    add /build to json

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output:{
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, '/build'),
         filename: 'index.bundle.js',
         publicPath: '/',
         // assetModuleFilename: "images/[name].[ext][query]",
