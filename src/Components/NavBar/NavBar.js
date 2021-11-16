9968d29a763f6d89fbc097077e7662be59caf302
someshvk <svkhandelwal655@gmail.com>
2021-11-16 20:30:08 +0530

    edit navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -30,7 +30,7 @@ const NavBar = () =>{
                 </span>
                 <span className="socialMedia">
                     <ul>
-                        <li><a href="mailto:svkhandelwal655@gmail.com" onClick={copyToClipboard}><i className="fas fa-at" aria-hidden="true"></i> </a></li>
+                        <li><a rel="noopener noreferrer" target="_blank" href="mailto:svkhandelwal655@gmail.com" onClick={copyToClipboard}><i className="fas fa-at" aria-hidden="true"></i> </a></li>
                         <li><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/somesh-khandelwal-269755195/"><i className="fab fa-linkedin" aria-hidden="true"></i> </a></li>
                         <li><a rel="noopener noreferrer" target="_blank" href="https://twitter.com/someshvk"><i className="fab fa-twitter" aria-hidden="true"></i> </a></li>
                         <li><a rel="noopener noreferrer" target="_blank" href="https://github.com/someshvk"><i className="fab fa-github" aria-hidden="true"></i> </a></li>
