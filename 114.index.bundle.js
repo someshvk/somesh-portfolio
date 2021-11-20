12092a41b187a8997eda2eef0a97d90a6f129bfa
someshvk <svkhandelwal655@gmail.com>
2021-11-20 23:45:21 +0530

    Updates

--- 114.index.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksomesh_portfolio=self.webpackChunksomesh_portfolio||[]).push([[114],{114:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});var n=r(294);const t=function(){return n.createElement("div",{className:"spinner"},n.createElement("div",{className:"spinner__border"},n.createElement("div",{className:"spinner__core"})))}}}]);
